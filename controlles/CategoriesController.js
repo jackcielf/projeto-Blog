37376f0d715ed70be3dcebb19128f826e06ce79f
JakkiFx <jackcielfelix555@gmail.com>
2024-01-13 19:22:47 -0300

    Salvando categorias no banco de dados

--- controlles/CategoriesController.js
+++ controlles/CategoriesController.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const Category = require('../models/Category');
+const slugify = require('slugify');
 
 router.get("/admin/categories/new", (req, res) => {
   res.render("admin/categories/new");
+});
+
+router.post('/categories/save', (req, res) => {
+  var title = req.body.title;
+  
+  if (title) {
+    Category.create({
+      title: title,
+      slug: slugify(title)
+    }).then(() => {
+      res.redirect('/');
+    })
+  } else {
+    res.redirect('/admin/categories/new');
+  }
+
 });
 
 module.exports = router;
