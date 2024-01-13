dcb080f1841ed202a391908025d150fa749c3b08
JakkiFx <jackcielfelix555@gmail.com>
2024-01-13 19:54:27 -0300

    Listado as categorias em uma tabela para admins

--- controlles/CategoriesController.js
+++ controlles/CategoriesController.js
@@ -1,26 +1,33 @@
 const express = require("express");
 const router = express.Router();
-const Category = require('../models/Category');
-const slugify = require('slugify');
+const Category = require("../models/Category");
+const slugify = require("slugify");
 
 router.get("/admin/categories/new", (req, res) => {
   res.render("admin/categories/new");
 });
 
-router.post('/categories/save', (req, res) => {
+router.post("/categories/save", (req, res) => {
   var title = req.body.title;
-  
+
   if (title) {
     Category.create({
       title: title,
-      slug: slugify(title)
+      slug: slugify(title),
     }).then(() => {
-      res.redirect('/');
-    })
+      res.redirect("/");
+    });
   } else {
-    res.redirect('/admin/categories/new');
+    res.redirect("/admin/categories/new");
   }
+});
 
+router.get("/admin/categories", (req, res) => {
+  Category.findAll().then((categories) => {
+    res.render("admin/categories/index", {
+      categories: categories,
+    });
+  });
 });
 
 module.exports = router;
