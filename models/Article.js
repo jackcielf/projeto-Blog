4c3f96c53f2395df2eeba309d505a64c11f80c3b
JakkiFx <jackcielfelix555@gmail.com>
2024-01-13 12:29:34 -0300

    Feito o relacionamento entre as tabelas

--- models/Article.js
+++ models/Article.js
@@ -1,5 +1,6 @@
 const Sequelize = require("sequelize");
 const connection = require("../database/db");
+const Category = require('./Category');
 
 const NAME_TABLE = "tb_articles";
 
@@ -17,5 +18,10 @@ const Article = connection.define(NAME_TABLE, {
     allowNull: false,
   },
 });
+
+Category.hasMany(Article); // UMA categoria has many artigos
+Article.belongsTo(Category); // UM artigo belongs to UMA categoria
+
+// Article.sync({ force: true });
 
 module.exports = Article;
