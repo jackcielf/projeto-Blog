7f475dd43e7545673598c445b290073e3ae20ff0
JakkiFx <jackcielfelix555@gmail.com>
2024-01-13 20:35:37 -0300

    Criado o sistema de deletar categoria

--- controlles/CategoriesController.js
+++ controlles/CategoriesController.js
@@ -30,4 +30,26 @@ router.get("/admin/categories", (req, res) => {
   });
 });
 
+router.post('/categories/delete', (req, res) => {
+  var id = req.body.id;
+
+  if (id) {
+    if (!isNaN(id)) {
+      
+      Category.destroy({
+        where: { id: id }
+      }).then(() => {
+        res.redirect('/admin/categories');
+      });
+
+    } else {
+      res.redirect('/admin/categories');
+    }
+  } else {
+    res.redirect('/admin/categories');
+  }
+
+
+});
+
 module.exports = router;
