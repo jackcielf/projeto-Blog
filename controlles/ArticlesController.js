65a9c38d8cc0fef3818b45c34de361fd633462b0
JakkiFx <jackcielfelix555@gmail.com>
2024-01-13 23:15:46 -0300

    Mostrando select, no form de artigos, com as categorias disponiveis

--- controlles/ArticlesController.js
+++ controlles/ArticlesController.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("../models/Category");
+const Articles = require("../models/Articles");
+const slugify = require('slugify');
 
 router.get("/admin/articles/new", (req, res) => {
-  res.render("admin/articles/new");
+  Category.findAll().then((categories) => {
+    res.render("admin/articles/new", {
+      categories: categories,
+    });
+  });
 });
 
 module.exports = router;
